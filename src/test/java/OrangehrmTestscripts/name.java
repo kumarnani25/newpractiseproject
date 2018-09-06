7b47c61113bc4f50801de94a0fef48a8bcf22885
unknown <naveen.nani25@gmail.com>
2018-09-06 21:36:24 +0530

    commited in branch

--- src/test/java/OrangehrmTestscripts/name.java
+++ src/test/java/OrangehrmTestscripts/name.java
@@ -9,7 +9,9 @@ public class name {
 	{
 		System.out.println("Name3213456890");
 		System.out.println("testing");
-          System.out.println("testing654321");
+          System.out.println("testing654321"); 
+
+/* created testbranch*/
 	}
 
 }
